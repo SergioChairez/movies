de3964a693abe21decc5b51e449be58cafc3e874
sergiochairez <chairez1934@gmail.com>
2023-03-03 12:49:09 -0600

    Added Hero Animation

--- lib/widgets/movie_slider.dart
+++ lib/widgets/movie_slider.dart
@@ -65,7 +65,7 @@ class _MovieSliderState extends State<MovieSlider> {
               controller: scrollController,
               scrollDirection: Axis.horizontal,
               itemCount: widget.movies.length,
-                itemBuilder:( _, int index ) => _MoviePoster( widget.movies[index] )
+                itemBuilder:( _, int index ) => _MoviePoster( widget.movies[index], '${ widget.title }-${ index }-${ widget.movies[index].id }' )
             ),
           )
 
@@ -78,11 +78,15 @@ class _MovieSliderState extends State<MovieSlider> {
 class _MoviePoster extends StatelessWidget {
 
   final Movie movie;
+  final String heroId;
 
-  const _MoviePoster( this.movie );
+  const _MoviePoster( this.movie, this.heroId );
 
   @override
   Widget build(BuildContext context) {
+
+    movie.heroId = heroId;
+
     return Container(
       width: 130, height: 190,
       margin: const EdgeInsets.symmetric( horizontal: 10 ),
@@ -91,14 +95,17 @@ class _MoviePoster extends StatelessWidget {
 
           GestureDetector(
             onTap: () => Navigator.pushNamed(context, 'details', arguments: movie ),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular( 20 ),
-              child: FadeInImage(
-                placeholder: const AssetImage('assets/no-image.jpg'),
-                image: NetworkImage( movie.fullPosterImg ),
-                width: 130,
-                height: 190,
-                fit: BoxFit.cover,
+            child: Hero(
+              tag: movie.heroId!,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular( 20 ),
+                child: FadeInImage(
+                  placeholder: const AssetImage('assets/no-image.jpg'),
+                  image: NetworkImage( movie.fullPosterImg ),
+                  width: 130,
+                  height: 190,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
